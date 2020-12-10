9afad2e997209b8b33f4bcba2faf3e09ecaed2cb
dallasclarke <dallas.ag.clarke@gmail.com>
2020-12-10 14:03:41 -0800

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import {TodoContext} from "./context/context"
+
+const Todo = () => {
+  
+  const {
+    todoList: { todo, id, isCompleted },
+    removeTodoByID,
+    todoDoneByID,
+  } = useContext(TodoContext); 
+
+  return (
+    <div style={{ textDecoration: isCompleted ? "line-through" : "" }}>
+      {todo}
+      <div>
+        <button onClick={() => todoDoneByID(id)}>Done</button>
+        <button onClick={() => removeTodoByID(id)}>Delete</button>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Todo;
